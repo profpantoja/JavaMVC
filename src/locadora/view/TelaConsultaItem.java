d6e176737abdae033d068ae91ec593a008b8257e
Tielle Alexandre <tiellesa@gmail.com>
2021-03-01 16:36:18 -0300

    Adicionar tela de consulta de item.

--- src/locadora/view/TelaConsultaItem.java
+++ src/locadora/view/TelaConsultaItem.java
@@ -23,17 +23,114 @@ public class TelaConsultaItem extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanelConsultaItem = new javax.swing.JPanel();
+        jLabelTitulo = new javax.swing.JLabel();
+        jLabelTituloFilme = new javax.swing.JLabel();
+        jTextFieldTituloFilme = new javax.swing.JTextField();
+        javax.swing.JButton jButtonConsultarItem = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableConsultarItem = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Video Locadora Hora da Pipoca");
+        setAlwaysOnTop(true);
+
+        jPanelConsultaItem.setBackground(new java.awt.Color(102, 102, 102));
+
+        jLabelTitulo.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
+        jLabelTitulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/icon.png"))); // NOI18N
+        jLabelTitulo.setText("Consulta de Item");
+
+        jLabelTituloFilme.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabelTituloFilme.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelTituloFilme.setText("Informe o título do filme:");
+
+        jTextFieldTituloFilme.setFont(new java.awt.Font("Calibri", 0, 12)); // NOI18N
+
+        jButtonConsultarItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/lupa.png"))); // NOI18N
+
+        jTableConsultarItem.setFont(new java.awt.Font("Calibri", 0, 12)); // NOI18N
+        jTableConsultarItem.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "Código Item", "Código Filme", "Título", "Tipo", "Preço"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTableConsultarItem);
+
+        javax.swing.GroupLayout jPanelConsultaItemLayout = new javax.swing.GroupLayout(jPanelConsultaItem);
+        jPanelConsultaItem.setLayout(jPanelConsultaItemLayout);
+        jPanelConsultaItemLayout.setHorizontalGroup(
+            jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                .addGroup(jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                        .addGap(268, 268, 268)
+                        .addComponent(jLabelTitulo))
+                    .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addGroup(jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 673, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                                .addComponent(jLabelTituloFilme)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jTextFieldTituloFilme, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButtonConsultarItem, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+        jPanelConsultaItemLayout.setVerticalGroup(
+            jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButtonConsultarItem, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanelConsultaItemLayout.createSequentialGroup()
+                        .addComponent(jLabelTitulo)
+                        .addGap(23, 23, 23)
+                        .addGroup(jPanelConsultaItemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabelTituloFilme)
+                            .addComponent(jTextFieldTituloFilme, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(59, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(jPanelConsultaItem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(46, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addComponent(jPanelConsultaItem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(47, Short.MAX_VALUE))
         );
 
         pack();
@@ -75,6 +172,12 @@ public class TelaConsultaItem extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JLabel jLabelTituloFilme;
+    private javax.swing.JPanel jPanelConsultaItem;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableConsultarItem;
+    private javax.swing.JTextField jTextFieldTituloFilme;
     // End of variables declaration//GEN-END:variables
 
 }
